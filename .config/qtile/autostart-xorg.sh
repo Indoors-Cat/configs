2212770af1c742635d6b149661fb39ee9881a069
Indoors-Cat <43878134+Indoors-Cat@users.noreply.github.com>
2023-06-19 23:21:53 -0400

    Added autostart for X11

---
+++ .config/qtile/autostart-xorg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Session:
+lxsession &
+# Wallpapers:
+nitrogen --restore &
+# Compositor:
+picom &
+#picom --experimental-backend &  #This is being used with picom-jonaburg-git
+#/usr/bin/emacs --daemon &
+#playerctl daemon &
